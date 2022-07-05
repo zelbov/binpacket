e9dc53960655938550a5f3601ef10523ab09eec6
Zelbov <jimborecard@gmail.com>
2022-07-05 11:20:45 +0300

    Updated exports to support tree shaking if needed

--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,15 @@
-import { Int8 } from "./decorators/property/Int8";
-import { Int16 } from "./decorators/property/Int16";
-import { Int32 } from "./decorators/property/Int32";
-import { Int64 } from "./decorators/property/Int64";
-import { parseBinary } from "./parser/Parser";
-import { serializeBinary } from "./builder/Builder";
-import { NestedBinary } from "./decorators/property/Nested";
-import { BinaryString } from './decorators/property/String'
-import { BinaryHeader, BinaryHeaderParser, BinaryHeaderSerializer } from "./decorators/class/BinaryHeader";
-import { Float32 } from "./decorators/property/Float32";
-import { Float64 } from "./decorators/property/Float64";
-import { BinaryArray } from "./decorators/property/Array";
-import { BinarySelector, identifyBinary } from "./decorators/class/BinarySelector";
-import { BinaryWriteStream } from "./streams/BinaryStreamWriter";
-import { BinaryReadStream } from "./streams/BinaryStreamReader";
-
-export {
-
-    // Class decorators
-    BinaryHeader,
-    BinarySelector,
-    
-    // Property decorators
-    Int8,
-    Int16,
-    Int32,
-    Int64,
-    Float32,
-    Float64,
-
-    BinaryString,
-
-    NestedBinary,
-
-    BinaryArray,
-
-    // Common functions
-    parseBinary,
-    serializeBinary,
-    identifyBinary,
-
-    // Streams
-    BinaryWriteStream,
-    BinaryReadStream,
-
-    // Types
-    BinaryHeaderParser,
-    BinaryHeaderSerializer
-
-}
+export { Int8 } from "./decorators/property/Int8";
+export { Int16 } from "./decorators/property/Int16";
+export { Int32 } from "./decorators/property/Int32";
+export { Int64 } from "./decorators/property/Int64";
+export { parseBinary } from "./parser/Parser";
+export { serializeBinary } from "./builder/Builder";
+export { NestedBinary } from "./decorators/property/Nested";
+export { BinaryString } from './decorators/property/String'
+export { BinaryHeader, BinaryHeaderParser, BinaryHeaderSerializer } from "./decorators/class/BinaryHeader";
+export { Float32 } from "./decorators/property/Float32";
+export { Float64 } from "./decorators/property/Float64";
+export { BinaryArray } from "./decorators/property/Array";
+export { BinarySelector, identifyBinary } from "./decorators/class/BinarySelector";
+export { BinaryWriteStream } from "./streams/BinaryStreamWriter";
+export { BinaryReadStream } from "./streams/BinaryStreamReader";
